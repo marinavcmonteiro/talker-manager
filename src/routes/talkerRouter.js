507d3c0f2d515746ef55e2a6cb25f65a0e092d79
Marina Monteiro <marina.vcmonteiro@gmail.com>
2023-11-24 11:46:50 -0300

    feat: Cria o endpoint GET /talker/:id

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -13,4 +13,18 @@ talkerRouter.get('/', async (_req, res) => {
   return res.status(200).json(data);
 });
 
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const response = await fs.readFile(filePath, 'utf-8');
+  const data = JSON.parse(response);
+
+  const talkerById = data.find((talker) => talker.id === Number(id));
+
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(talkerById);
+});
+
 module.exports = talkerRouter;
